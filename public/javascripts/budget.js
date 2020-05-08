f544e6feb550888e19c76e86eedb68e12abdba3a
vonnenaut <danielashcom@gmail.com>
2020-05-08 11:01:45 +0700

    u/d budget.js To-Do: add pop-up warning when category name is that of an existing category and will overwrite data

--- public/javascripts/budget.js
+++ public/javascripts/budget.js
@@ -24,27 +24,4 @@ $(function() {
     }
   });
 
-  $("a.edit").submit(function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    var ok = confirm("Category exists with this name and it will be overwritten. Are you sure? This cannot be undone!");
-    if (ok) {
-      var form = $(this);
-
-      var request = $.ajax({
-        url: form.attr("action"),
-        method: form.attr("method")
-      });
-
-      request.done(function(data, textStatus, jqXHR) {
-        if (jqXHR.status === 204) {
-          form.parent("tr").remove();
-        } else if (jqXHR.status === 200) {
-          document.location = data;
-        }
-      });
-    }
-  });
-
 });
